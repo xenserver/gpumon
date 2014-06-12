a95a2b5998d0b386ca9f0d2db8910e0717242258
John Else <john.else@citrix.com>
2014-06-12 16:33:56 +0100

    Add config.mli
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ lib/config.mli
@@ -0,0 +1,26 @@
+(* Metrics which require calling nvmlDeviceGetMemoryInfo *)
+type memory_metric =
+	| Free
+	| Used
+
+(* Metrics which have their own NVML calls. *)
+type other_metric =
+	| PowerUsage
+	| Temperature
+
+(* Metrics which require calling nvmlDeviceGetUtilizationRates *)
+type utilisation_metric =
+	| Compute
+	| MemoryIO
+
+type metric =
+	| Memory of memory_metric
+	| Utilisation of utilisation_metric
+	| Other of other_metric
+
+type config = ((int32 * metric list) list)
+
+val of_string : string -> [`Ok of config | `Parse_failure of string]
+
+val of_file : string ->
+	[`Ok of config | `Parse_failure of string | `Does_not_exist]
