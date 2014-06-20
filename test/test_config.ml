808fe2575f96885746641001101ac447121ff4b5
John Else <john.else@citrix.com>
2014-06-20 03:47:53 +0100

    Add tests of the v2 config format parsing
    Signed-off-by: John Else <john.else@citrix.com>

--- test/test_config.ml
+++ test/test_config.ml
@@ -14,41 +14,122 @@ let test_file config_file expected_result =
 		~printer:string_of_result
 		expected_result actual_result
 
+let default_config =
+	let open Config in
+	{
+		device_types = [
+			(* GRID K1 *)
+			{
+				device_id = 0x0ff2l;
+				subsystem_device_id = Any;
+				metrics = [
+					Memory Free;
+					Memory Used;
+					Other Temperature;
+					Other PowerUsage;
+					Utilisation Compute;
+					Utilisation MemoryIO;
+				];
+			};
+			(* GRID K2 *)
+			{
+				device_id = 0x11bfl;
+				subsystem_device_id = Any;
+				metrics = [
+					Memory Free;
+					Memory Used;
+					Other Temperature;
+					Other PowerUsage;
+					Utilisation Compute;
+					Utilisation MemoryIO;
+				];
+			};
+		];
+	}
+
+let default_with_match_config =
+	let open Config in
+	{
+		device_types = [
+			(* GRID K1 *)
+			{
+				device_id = 0x0ff2l;
+				subsystem_device_id = Match 0x1012l;
+				metrics = [
+					Memory Free;
+					Memory Used;
+					Other Temperature;
+					Other PowerUsage;
+					Utilisation Compute;
+					Utilisation MemoryIO;
+				];
+			};
+			(* GRID K2 *)
+			{
+				device_id = 0x11bfl;
+				subsystem_device_id = Match 0x100al;
+				metrics = [
+					Memory Free;
+					Memory Used;
+					Other Temperature;
+					Other PowerUsage;
+					Utilisation Compute;
+					Utilisation MemoryIO;
+				];
+			};
+		];
+	}
+
+let subsystem_device_id_config =
+	let open Config in
+	{
+		device_types = [
+			{
+				device_id = 0x1234l;
+				subsystem_device_id = Match 0x5687l;
+				metrics = [
+					Memory Free;
+					Memory Used;
+				];
+			};
+		];
+	}
+
+let v2_mixed_config =
+	let open Config in
+	{
+		device_types = [
+			{
+				device_id = 0x1234l;
+				subsystem_device_id = Any;
+				metrics = [
+					Other Temperature;
+					Other PowerUsage;
+				];
+			};
+			{
+				device_id = 0x5678l;
+				subsystem_device_id = Match 0x9abcl;
+				metrics = [
+					Utilisation Compute;
+					Utilisation MemoryIO;
+				];
+			};
+		];
+	}
+
 let tests =
 	let open Config in
 	[
 		"test_does_not_exist.conf", `Error `Does_not_exist;
-		"test_default.conf", `Ok {
-			device_types = [
-				(* GRID K1 *)
-				{
-					device_id = 0x0ff2l;
-					subsystem_device_id = Any;
-					metrics = [
-						Memory Free;
-						Memory Used;
-						Other Temperature;
-						Other PowerUsage;
-						Utilisation Compute;
-						Utilisation MemoryIO;
-					];
-				};
-				(* GRID K2 *)
-				{
-					device_id = 0x11bfl;
-					subsystem_device_id = Any;
-					metrics = [
-						Memory Free;
-						Memory Used;
-						Other Temperature;
-						Other PowerUsage;
-						Utilisation Compute;
-						Utilisation MemoryIO;
-					];
-				};
-			];
-		};
-		"test_minimal.conf", `Ok {device_types = []};
+		"test_unknown_version.conf", `Error (`Unknown_version "\"4\"");
+		"test_v1_minimal.conf", `Ok {device_types = []};
+		"test_v2_minimal.conf", `Ok {device_types = []};
+		"test_v1_default.conf", `Ok default_config;
+		"test_v2_default.conf", `Ok default_config;
+		"test_v2_default_with_match.conf", `Ok default_with_match_config;
+		"test_v2_with_subsystem_device_id.conf", `Ok subsystem_device_id_config;
+		"test_v2_mixed.conf", `Ok v2_mixed_config;
 	]
 
 let test =
