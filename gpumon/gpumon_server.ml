79994c4d87e1001252f4552d7e4c888667650f36
Marcello Seri <marcello.seri@citrix.com>
2017-04-25 13:56:40 +0100

    gpumon_server: namespace nvidia-only interface
    This should avoid confusion and prevent potential name clashes if similar features are implemented for other manufacturers.
    
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- gpumon/gpumon_server.ml
+++ gpumon/gpumon_server.ml
@@ -19,71 +19,77 @@ module Make(I: Interface) = struct
   module D = Debug.Make(struct let name = Gpumon_interface.service_name end)
   open D
 
-  let get_interface_exn () = 
-    match I.interface with
-    | Some interface -> interface
-    | None -> raise Gpumon_interface.NvmlInterfaceNotAvailable
-
   type context = unit
 
-  let get_pgpu_metadata _ dbg pgpu_address =
-    let interface = get_interface_exn () in
-    try
-      let device = Nvml.device_get_handle_by_pci_bus_id interface pgpu_address in
-      Nvml.device_get_pgpu_metadata interface device
-    with err -> raise (Gpumon_interface.NvmlFailure (Printexc.to_string err))
+  module Nvidia = struct
+    
+    exception InterfaceNotAvailable
+    exception Failure of string
 
-  let get_pgpu_vm_compatibility _ dbg pgpu_address domid pgpu_metadata =
-    let interface = get_interface_exn () in
-    let compatibility = 
+    let get_interface_exn () = 
+    match I.interface with
+    | Some interface -> interface
+    | None -> raise InterfaceNotAvailable
+
+    let get_pgpu_metadata _ dbg pgpu_address =
+      let interface = get_interface_exn () in
       try
-        (* Return a tuple vm_compat, pgpu_compat_limit for convenience:
-         * we have List helpers that later help to split the list *)
-        let vgpu_to_compat vgpu =
-          let vgpu_compat =
-            Nvml.get_pgpu_vgpu_compatibility interface vgpu pgpu_metadata 
-          in 
-          ( Nvml.vgpu_compat_get_vm_compat vgpu_compat
-          , Nvml.vgpu_compat_get_pgpu_compat_limit vgpu_compat) 
-        in
-        let current_device = 
-          Nvml.device_get_handle_by_pci_bus_id interface pgpu_address
-        in
-        let vgpus =
-          Nvml.get_vgpus_for_vm interface current_device (string_of_int domid)
-        in
-        List.map vgpu_to_compat vgpus
-      with err -> 
-        raise (Gpumon_interface.NvmlFailure (Printexc.to_string err))
-    in
-    match compatibility with
-    | [] ->
-      (* We call this function when we expect the VM to have a vGPU, 
-       * if this is not the case we consider it an internal error *)
-      failwith (Printf.sprintf "No vGPU available for the VM with domid %d" domid)
-    | compatiblity ->
-      let open Stdext.Listext in
-      let vm_compat, limits = List.split compatibility in
-      let support_live_migration =
-        vm_compat
-        |> List.map (List.mem Nvml.Live)
-        |> List.fold_left (&&) true
+        let device = Nvml.device_get_handle_by_pci_bus_id interface pgpu_address in
+        Nvml.device_get_pgpu_metadata interface device
+      with err -> raise (Failure (Printexc.to_string err))
+
+    let get_pgpu_vm_compatibility _ dbg pgpu_address domid pgpu_metadata =
+      let interface = get_interface_exn () in
+      let compatibility = 
+        try
+          (* Return a tuple vm_compat, pgpu_compat_limit for convenience:
+          * we have List helpers that later help to split the list *)
+          let vgpu_to_compat vgpu =
+            let vgpu_compat =
+              Nvml.get_pgpu_vgpu_compatibility interface vgpu pgpu_metadata 
+            in 
+            ( Nvml.vgpu_compat_get_vm_compat vgpu_compat
+            , Nvml.vgpu_compat_get_pgpu_compat_limit vgpu_compat) 
+          in
+          let current_device = 
+            Nvml.device_get_handle_by_pci_bus_id interface pgpu_address
+          in
+          let vgpus =
+            Nvml.get_vgpus_for_vm interface current_device (string_of_int domid)
+          in
+          List.map vgpu_to_compat vgpus
+        with err -> 
+          raise (Failure (Printexc.to_string err))
       in
-      let failures =
-        limits
-        |> List.filter ((<>) [Nvml.None])
-        |> List.concat
-        |> List.setify
-        |> List.map (function
-            | Nvml.HostDriver  -> Gpumon_interface.Host_driver
-            | Nvml.GuestDriver -> Gpumon_interface.Guest_driver
-            | Nvml.GPU         -> Gpumon_interface.GPU
-            | Nvml.Other       -> Gpumon_interface.Other
-            (* NOTE: replace this failwith with `-> .` 
-             * after we upgrade the compiler *)
-            | Nvml.None        -> failwith "This should never happen"
-          ) in
-      match support_live_migration, failures with
-      | true, [] -> Gpumon_interface.Compatible
-      | _, _ -> Gpumon_interface.(Incompatible failures)
+      match compatibility with
+      | [] ->
+        (* We call this function when we expect the VM to have a vGPU, 
+        * if this is not the case we consider it an internal error *)
+        failwith (Printf.sprintf "No vGPU available for the VM with domid %d" domid)
+      | compatiblity ->
+        let open Stdext.Listext in
+        let vm_compat, limits = List.split compatibility in
+        let support_live_migration =
+          vm_compat
+          |> List.map (List.mem Nvml.Live)
+          |> List.fold_left (&&) true
+        in
+        let failures =
+          limits
+          |> List.filter ((<>) [Nvml.None])
+          |> List.concat
+          |> List.setify
+          |> List.map (function
+              | Nvml.HostDriver  -> Gpumon_interface.Host_driver
+              | Nvml.GuestDriver -> Gpumon_interface.Guest_driver
+              | Nvml.GPU         -> Gpumon_interface.GPU
+              | Nvml.Other       -> Gpumon_interface.Other
+              (* NOTE: replace this failwith with `-> .` 
+              * after we upgrade the compiler *)
+              | Nvml.None        -> failwith "This should never happen"
+            ) in
+        match support_live_migration, failures with
+        | true, [] -> Gpumon_interface.Compatible
+        | _, _ -> Gpumon_interface.(Incompatible failures)
+  end
 end
